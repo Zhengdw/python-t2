2fdaeef3f99443efa810931b5c4d9d06bebfd7b3
JonTheSwimmer <j.zhang.1@alumni.ubc.ca>
2015-02-07 12:43:29 -0800

    Added in the code from Thursday

--- test.py
+++ test.py
@@ -5,3 +5,21 @@ b=a-33
 c="Hello"
 
 print(a+b,c)
+
+h = 6.62606957 * 10 ** -34
+#m = 1.67492735174 * 10 ** -27
+m = 9.11 * 10**-31
+L = 0.39 * 10**-9
+convert  = 6.24150934 * 10**18
+
+energylevels = []
+
+def energy(n):
+    return ((n*h/L)**2)/(8*m)*convert
+
+for a in range(1,10000):
+    energylevels.append(round(energy(a),4))
+
+
+def energyassign(n):
+    
