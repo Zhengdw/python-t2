e07c180dce80d98e8ad9bd755730ab83d0c5e0a1
David Zheng <zhengdwei@gmail.com>
2015-02-12 13:11:43 -0800

    Updated with start of better functions

--- One neutron model.py
+++ One neutron model.py
@@ -1,13 +1,11 @@
 import numpy as np
 from math import factorial
 from itertools import combinations
-from visual import *
-from visual.graph import *
 
-h = 6.62606957 * 10 ** -34
+h = 6.62606957 * 10 ** -34      #Plank's constant
 #m = 1.67492735174 * 10 ** -27   #this is mass of neutron
 m = 9.11 * 10**-31             #this is mass of electron
-L = 0.39 * 10**-9
+L = 0.39 * 10**-9               #size of box
 convert  = 6.24150934 * 10**18
 maximum = 100
 
@@ -16,6 +14,68 @@ energylevels = np.fromiter((((x*h/L)**2)/(8*m)*convert for x in range(100)),dtyp
 
 def energy(n):
     return ((n*h/L)**2)/(8*m)*convert
+
+def cartesian(arrays, out=None):
+    """
+    Generate a cartesian product of input arrays.
+
+    Parameters
+    ----------
+    arrays : list of array-like
+        1-D arrays to form the cartesian product of.
+    out : ndarray
+        Array to place the cartesian product in.
+
+    Returns
+    -------
+    out : ndarray
+        2-D array of shape (M, len(arrays)) containing cartesian products
+        formed of input arrays.
+
+    Examples
+    --------
+    >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
+    array([[1, 4, 6],
+           [1, 4, 7],
+           [1, 5, 6],
+           [1, 5, 7],
+           [2, 4, 6],
+           [2, 4, 7],
+           [2, 5, 6],
+           [2, 5, 7],
+           [3, 4, 6],
+           [3, 4, 7],
+           [3, 5, 6],
+           [3, 5, 7]])
+
+    """
+
+    arrays = [np.asarray(x) for x in arrays]
+    dtype = arrays[0].dtype
+ 
+    n = np.prod([x.size for x in arrays])
+    if out is None:
+        out = np.zeros([n, len(arrays)], dtype=dtype)
+
+    m = n / arrays[0].size
+    out[:,0] = np.repeat(arrays[0], m)
+    if arrays[1:]:
+        cartesian(arrays[1:], out=out[0:m,1:])
+        for j in xrange(1, arrays[0].size):
+            out[j*m:(j+1)*m,1:] = out[0:m,1:]
+    return out
+
+def combinations(array,n):
+    """
+    Take an array and output all combinations of n
+    """
+    
+"""
+def boson(n):
+    energyconfigs=cartesian([energylevel,energylevel,energylevel,energylevel])
+    print energyconfigs[1]
+
+"""
 
 def fermion(number):
     energycount = []
@@ -33,4 +93,4 @@ def fermion(number):
     fnc1.plot(data=energycount)
     return np.histogram(energycount,bins = 300,weights = None, density = False)
 
-# BTW, Jonathan, this isn't working for nuetron masses. Something to do with rounding
+
